cfad04410e127a26d71cfe6ce572bcfd5523dca3
vishal1142 <vishalmachan5@gmail.com>
2025-04-21 15:30:07 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-21 15:30:07

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -1,3 +1,10 @@
+// jenkinslibrary.groovy
+
 def call() {
-    sh 'mvn test'
+    try {
+        sh 'mvn test'
+    } catch (Exception e) {
+        echo "Maven Test Failed: ${e.getMessage()}"
+        throw e // Re-throw the exception to cause pipeline failure
+    }
 }
