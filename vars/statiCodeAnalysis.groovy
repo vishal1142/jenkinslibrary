f06ec9c3c45309382b4a80cb38d6dc53d80ba26c
vishal1142 <vishalmachan5@gmail.com>
2025-04-22 10:30:15 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-22 10:30:15

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,11 +1,12 @@
-def call(Map params) {
-    def sonarInstanceName = params.get('sonarInstanceName', 'SonarQube') // Use the name from Jenkins config
-    def sonarHostUrl = params.get('sonarHostUrl', 'http://localhost:9000')
+def call(Map params = [:]) {  // Provide a default empty map if none is passed
+    def credentialsId = params.credentialsId
+    def sonarHostUrl = params.get('sonarHostUrl', 'http://localhost:9000')  // Default URL is localhost if not provided
     def sonarProjectKey = params.get('sonarProjectKey', null)
     def sonarProjectName = params.get('sonarProjectName', null)
     def sonarProjectVersion = params.get('sonarProjectVersion', null)
 
-    withSonarQubeEnv(sonarInstanceName) {
+    withSonarQubeEnv(credentialsId: credentialsId) {
+        // Set the custom SonarQube host URL if provided
         env.SONAR_HOST_URL = sonarHostUrl
 
         sh 'echo "SonarQube host: $SONAR_HOST_URL"'
@@ -14,7 +15,7 @@ def call(Map params) {
         if (sonarProjectKey && sonarProjectName && sonarProjectVersion) {
             sonarCmd += " -Dsonar.projectKey=${sonarProjectKey} -Dsonar.projectName=${sonarProjectName} -Dsonar.projectVersion=${sonarProjectVersion}"
         }
-
+        
         sh sonarCmd
     }
 }
