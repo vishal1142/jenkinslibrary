5890b28dfcc29fecdecc1370ccd6748d9c6a77b4
vishal1142 <vishalmachan5@gmail.com>
2025-04-22 01:49:26 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-22 01:49:25

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,11 +1,17 @@
-def call(credentialsId){
+def call(Map params) {
+    def credentialsId = params.credentialsId
+    def sonarProjectKey = params.get('sonarProjectKey', null)
+    def sonarProjectName = params.get('sonarProjectName', null)
+    def sonarProjectVersion = params.get('sonarProjectVersion', null)
+
     withSonarQubeEnv(credentialsId: credentialsId) {
         sh 'echo "SonarQube host: $SONAR_HOST_URL"'
-        sh 'mvn clean package sonar:sonar'
+        
+        def sonarCmd = 'mvn clean package sonar:sonar'
+        if (sonarProjectKey && sonarProjectName && sonarProjectVersion) {
+            sonarCmd += " -Dsonar.projectKey=${sonarProjectKey} -Dsonar.projectName=${sonarProjectName} -Dsonar.projectVersion=${sonarProjectVersion}"
+        }
+        
+        sh sonarCmd
     }
 }
-def call(credentialsId, sonarProjectKey, sonarProjectName, sonarProjectVersion){
-    withSonarQubeEnv(credentialsId: credentialsId) {
-        sh 'echo "SonarQube host: $SONAR_HOST_URL"'
-        sh "mvn clean package sonar:sonar -Dsonar.projectKey=${sonarProjectKey} -Dsonar.projectName=${sonarProjectName} -Dsonar.projectVersion=${sonarProjectVersion}"
-    }
