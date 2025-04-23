b56f2387a0e14d17dc0da644c86ca712b94c007e
vishal1142 <vishalmachan5@gmail.com>
2025-04-23 16:51:41 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-23 16:51:41

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,9 +1,3 @@
-def call() {
-    echo "[Build] Starting Maven build..."
-
-    withEnv(["JAVA_HOME=${tool 'JDK11'}", "PATH+MAVEN=${tool 'Maven3'}/bin"]) {
-        sh 'mvn clean install -DskipTests=false'
-    }
-
-    echo "[Build] Maven build completed successfully."
+def call(){
+    sh 'mvn clean install'
 }
