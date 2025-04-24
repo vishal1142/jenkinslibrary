5e554b412ffac0f58911732cb8840be3c26357af
vishal1142 <vishalmachan5@gmail.com>
2025-04-23 23:32:25 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-23 23:32:25

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,10 @@
 def call(Map args) {
-    def project = args.project
-    def imageTag = args.imageTag
-    def hubUser = args.hubUser
+    def project = args.project ?: error("Missing parameter: project")
+    def imageTag = args.imageTag ?: "latest"
+    def hubUser = args.hubUser ?: error("Missing parameter: hubUser")
 
     sh """
-        docker image build -t ${hubUser}/${project} .
-        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+        docker image build -t ${hubUser}/${project}:${imageTag} .
+        docker image tag ${hubUser}/${project}:${imageTag} ${hubUser}/${project}:latest
     """
 }
