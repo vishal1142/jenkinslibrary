b3852d31cc64ca7ecf23c734f9b965cbfbf42551
vishal1142 <vishalmachan5@gmail.com>
2025-04-23 23:46:45 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-23 23:46:45

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,10 @@
 def call(Map args) {
-    def project = args.project ?: error("Missing parameter: project")
-    def imageTag = args.imageTag ?: "latest"
-    def hubUser = args.hubUser ?: error("Missing parameter: hubUser")
+    // Use parameter names that match the Jenkinsfile exactly
+    def project = args.ImageName ?: error("Missing parameter: ImageName")  // Corresponds to 'ImageName' in Jenkinsfile
+    def imageTag = args.ImageTag ?: "latest"  // Default to 'latest' if not provided, corresponds to 'ImageTag' in Jenkinsfile
+    def hubUser = args.DockerHubUser ?: error("Missing parameter: DockerHubUser")  // Corresponds to 'DockerHubUser' in Jenkinsfile
 
+    // Docker build and tagging commands
     sh """
         docker image build -t ${hubUser}/${project}:${imageTag} .
         docker image tag ${hubUser}/${project}:${imageTag} ${hubUser}/${project}:latest
