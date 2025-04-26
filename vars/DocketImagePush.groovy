549e928ae07c89244862891a34dfa0707bd3c138
vishal1142 <vishalmachan5@gmail.com>
2025-04-26 12:06:49 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-26 12:06:48

--- vars/DocketImagePush.groovy
+++ vars/DocketImagePush.groovy
@@ -1,11 +1,24 @@
-def call(String project, String ImageTag, String hubUser){
+def call(String project, String ImageTag, String hubUser) {
     withCredentials([usernamePassword(
-            credentialsId: "docker",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
+        credentialsId: 'vishal',
+        usernameVariable: 'USER',
+        passwordVariable: 'PASS'
     )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
+        sh """
+            echo "Logging into DockerHub with user: \$USER"
+            docker login -u "\$USER" -p "\$PASS"
+        """
     }
-    sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"   
+
+    def fullImageName = "${hubUser}/${project}:${ImageTag}"
+    def latestImageName = "${hubUser}/${project}:latest"
+
+    echo "Pushing Docker image: ${fullImageName}"
+    sh "docker push ${fullImageName}"
+
+    echo "Tagging image as latest: ${latestImageName}"
+    sh "docker tag ${fullImageName} ${latestImageName}"   // <--- THIS was missing
+
+    echo "Pushing Docker image: ${latestImageName}"
+    sh "docker push ${latestImageName}"
 }
