c03764dab58b21cfa08f47305c20c55e41500a18
vishal1142 <vishalmachan5@gmail.com>
2025-04-27 21:02:31 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-27 21:02:30

--- vars/dockerbuildECR.groovy
+++ vars/dockerbuildECR.groovy
@@ -1,24 +1,11 @@
-// EcrBuildPush.groovy
-def call(String awsAccountId, String region, String ecrRepoName, String imageTag) {
-    // Authenticate Docker to Amazon ECR
-    echo "Authenticating Docker to Amazon ECR..."
-    sh """
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${region}.amazonaws.com
-    """
-
-    // Build the Docker image
-    echo "Building Docker image..."
-    sh "docker build -t ${ecrRepoName}:${imageTag} ."
-
-    // Tag the Docker image
-    echo "Tagging Docker image..."
-    sh """
-        docker tag ${ecrRepoName}:${imageTag} ${awsAccountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:${imageTag}
-    """
-
-    // Push the Docker image to ECR
-    echo "Pushing Docker image to ECR..."
-    sh """
-        docker push ${awsAccountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:${imageTag}
-    """
+// vars/ecr.groovy
+def authenticateToECR(credentialsId, awsRegion, ecrRegistry) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        // Perform the AWS ECR login
+        sh """
+            aws ecr get-login-password --region ${awsRegion} | docker login --username ${DOCKER_USERNAME} --password-stdin ${ecrRegistry}
+        """
+    }
 }
+
+return this
