6d5f52b7f91f327833e2d112b19b6aea3c2fdc5b
vishal1142 <vishalmachan5@gmail.com>
2025-04-27 21:30:27 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-27 21:30:27

--- vars/dockerbuildECR.groovy
+++ vars/dockerbuildECR.groovy
@@ -1,11 +1,7 @@
-// vars/ecr.groovy
-def authenticateToECR(credentialsId, awsRegion, ecrRegistry) {
-    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-        // Perform the AWS ECR login
-        sh """
-            aws ecr get-login-password --region ${awsRegion} | docker login --username ${DOCKER_USERNAME} --password-stdin ${ecrRegistry}
-        """
-    }
-}
+def call(String aws_account_id, String region, String ecr_repoName){
 
-return this
+    sh """
+     docker build -t ${ecr_repoName} .
+     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
+}
