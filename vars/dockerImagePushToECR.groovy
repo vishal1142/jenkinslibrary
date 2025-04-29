cce474c019a03bffed81d5558245bc5093edd6e3
vishal1142 <vishalmachan5@gmail.com>
2025-04-29 16:23:26 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-29 16:23:26

--- vars/dockerImagePushToECR.groovy
+++ vars/dockerImagePushToECR.groovy
@@ -1,20 +1,23 @@
-def call(Map config) {
-    def imageName = config.ImageName
-    def imageTag = config.ImageTag
-    def accountId = config.AWS_ACCOUNT_ID
-    def region = config.REGION
-    def repoName = config.ECR_REPO_NAME
-
-    def fullImageName = "${accountId}.dkr.ecr.${region}.amazonaws.com/${repoName}:${imageTag}"
-
-    withCredentials([[
-        $class: 'AmazonWebServicesCredentialsBinding',
-        credentialsId: 'ecr-credentials'  // ID of the AWS credentials
-    ]]) {
+def DockerImagePushToECR(String project, String ImageTag, String ecrUrl, String region) {
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-creds']]) {
         sh """
-            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountId}.dkr.ecr.${region}.amazonaws.com
-            docker tag ${imageName}:${imageTag} ${fullImageName}
-            docker push ${fullImageName}
+            echo "Authenticating Docker with AWS ECR..."
+            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ecrUrl}
         """
     }
+
+    def fullImageName = "${ecrUrl}/${project}:${ImageTag}"
+    def latestImageName = "${ecrUrl}/${project}:latest"
+
+    echo "Tagging image: ${fullImageName}"
+    sh "docker tag ${project}:${ImageTag} ${fullImageName}"
+
+    echo "Pushing Docker image: ${fullImageName}"
+    sh "docker push ${fullImageName}"
+
+    echo "Tagging image as latest: ${latestImageName}"
+    sh "docker tag ${project}:${ImageTag} ${latestImageName}"
+
+    echo "Pushing Docker image: ${latestImageName}"
+    sh "docker push ${latestImageName}"
 }
