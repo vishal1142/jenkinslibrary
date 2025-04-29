6cffd9dbdf9632b5235226157565f06449ac8207
vishal1142 <vishalmachan5@gmail.com>
2025-04-29 18:32:30 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-29 18:32:28

--- vars/dockerCleanup.groovy
+++ vars/dockerCleanup.groovy
@@ -1,16 +1,13 @@
-def call() {
-    echo "Cleaning up dangling Docker images and unused resources..."
+def call(Map config = [:]) {
+    def imageName = config.ImageName ?: error("ImageName is required")
+    def imageTag = config.ImageTag ?: "latest"
 
-    sh '''
-        echo "Removing dangling Docker images..."
+    def fullImageName = "${imageName}:${imageTag}"
+
+    echo "Removing Docker image: ${fullImageName}"
+
+    sh """
+        docker rmi -f ${fullImageName} || echo 'Image not found or already removed.'
         docker image prune -f
-
-        echo "Removing unused Docker volumes..."
-        docker volume prune -f
-
-        echo "Removing unused Docker networks..."
-        docker network prune -f
-
-        echo "Cleanup completed."
-    '''
+    """
 }
