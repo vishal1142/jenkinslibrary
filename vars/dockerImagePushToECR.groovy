a2a451c62e7c92f3b0aa920dc63be7812718e41b
vishal1142 <vishalmachan5@gmail.com>
2025-04-28 23:11:52 -0500

    SHELL SCRIPTING Auto-commit on 2025-04-28 23:11:52

--- vars/dockerImagePushToECR.groovy
+++ vars/dockerImagePushToECR.groovy
@@ -1,17 +1,25 @@
 def call(Map config) {
+    // Extract required parameters from the config map
     def awsAccountId = config.awsAccountId
-    def region = config.region
-    def ecrRepoName = config.ecrRepoName
-    def imageName = config.imageName
-    def imageTag = config.imageTag
+    def region       = config.region
+    def ecrRepoName  = config.ecrRepoName
+    def imageName    = config.imageName
+    def imageTag     = config.imageTag
 
-    def fullEcrUrl = "${awsAccountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:${imageTag}"
+    // Construct the full ECR image URL
+    def fullEcrImageUrl = "${awsAccountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:${imageTag}"
 
+    // Tag and push the Docker image to ECR
     sh """
-        echo "Tagging Docker image..."
-        docker tag ${imageName}:${imageTag} ${fullEcrUrl}
+        echo "Tagging Docker image: ${imageName}:${imageTag} -> ${fullEcrImageUrl}"
+        docker tag ${imageName}:${imageTag} ${fullEcrImageUrl}
 
-        echo "Pushing Docker image to ECR..."
-        docker push ${fullEcrUrl}
+        echo "Authenticating with AWS ECR..."
+        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${region}.amazonaws.com
+
+        echo "Pushing Docker image to ECR repository..."
+        docker push ${fullEcrImageUrl}
+
+        echo "Docker image pushed successfully: ${fullEcrImageUrl}"
     """
 }
